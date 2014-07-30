23f4c70a365689f0ed91e1fe2fc3c9657fc7d23b
Sergii Iakymenko <sergii_iakymenko@epam.com>
2014-07-30 15:41:15 +0300

    initial commit

---
+++ iviti-rest/src/main/java/com/iviti/rest/controller/PingController.java
@@ -0,0 +1,18 @@
+package com.iviti.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String ping() {
+        return "pong";
+    }
+}
