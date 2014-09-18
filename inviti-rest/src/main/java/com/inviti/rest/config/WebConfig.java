8d6d4a254e597dd3dd44ce5d3971111118baf06f
Autopost <autopost@ukr.net>
2014-09-18 14:12:54 +0300

    Initial swagger doc-api implementation.

--- inviti-rest/src/main/java/com/inviti/rest/config/WebConfig.java
+++ inviti-rest/src/main/java/com/inviti/rest/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.inviti.rest.config;
 import com.inviti.repository.annotations.ProductionConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @ProductionConfig
+@Import(SwaggerConfig.class)
 @ComponentScan(basePackages = {"com.inviti.rest.controller", "com.inviti.repository", "com.inviti.service"})
 public class WebConfig extends WebMvcConfigurerAdapter {
     //Set default servlet handler, this is the same as <mvc:default-servlet-handler/>
