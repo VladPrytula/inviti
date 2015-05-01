4befb3257fad4532f28843586dd3c221d9564234
VladyslavPrytula <vladyslav.prytula@gmail.com>
2015-05-01 13:56:21 +0200

    added store image Test. TODO: clean DataStore structure

--- inviti-model/src/main/java/com/inviti/model/domainmodel/DataStore.java
+++ inviti-model/src/main/java/com/inviti/model/domainmodel/DataStore.java
@@ -6,48 +6,50 @@ import org.springframework.data.cassandra.mapping.Column;
 import org.springframework.data.cassandra.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.mapping.Table;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 /**
-
- CREATE KEYSPACE filestorage WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};
- USE events;
- CREATE TABLE DataStore (
- type text,
- bucket text,
- id timeuuid,
- tags set<text>,
- PRIMARY KEY ((type, bucket), id)
- ) WITH CLUSTERING ORDER BY (id DESC);
-
-
-
-
-
  * CREATE KEYSPACE filestorage WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};
- USE filestorage;
-
- CREATE TABLE IF NOT EXISTS data_store (
- commit blob, -- blob representing the commit hash
- delta int, -- how much the scores have changed
- score int, -- the test score, which is determined by the client
- test blob, -- blob for the test
- PRIMARY KEY(commit, delta, test)
- );
-
- insert into test_by_score  (commit, delta, test, score) values
- (textAsBlob('bdb14fbe076f6b94444c660e36a400151f26fc6f'), 0, textAsBlob('{"prefix": "enwiki", "title": "\"Aghnadarragh\""}'), 100
- );
-
- INSERT INTO test_by_score (commit, delta, test, score) VALUES (
- textAsBlob('cdb14fbe076f6b94444c660e36a400151f26fc6f'), 0, textAsBlob('{"prefix": "enwiki", "title": "\"Aghnadarragh\""}'), 100
- );
-
- insert into test_by_score (commit, delta, test, score) values (
- textAsBlob('adb14fbe076f6b94444c660e36a400151f26fc6f'), 0, textAsBlob('{"prefix": "enwiki", "title": "\"Aghnadarragh\""}'), 100
- );
+ * USE events;
+ * CREATE TABLE DataStore (
+ * type text,
+ * bucket text,
+ * id timeuuid,
+ * tags set<text>,
+ * PRIMARY KEY ((type, bucket), id)
+ * ) WITH CLUSTERING ORDER BY (id DESC);
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * CREATE KEYSPACE filestorage WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};
+ * USE filestorage;
+ * <p>
+ * CREATE TABLE IF NOT EXISTS data_store (
+ * commit blob, -- blob representing the commit hash
+ * delta int, -- how much the scores have changed
+ * score int, -- the test score, which is determined by the client
+ * test blob, -- blob for the test
+ * PRIMARY KEY(commit, delta, test)
+ * );
+ * <p>
+ * insert into test_by_score  (commit, delta, test, score) values
+ * (textAsBlob('bdb14fbe076f6b94444c660e36a400151f26fc6f'), 0, textAsBlob('{"prefix": "enwiki", "title": "\"Aghnadarragh\""}'), 100
+ * );
+ * <p>
+ * INSERT INTO test_by_score (commit, delta, test, score) VALUES (
+ * textAsBlob('cdb14fbe076f6b94444c660e36a400151f26fc6f'), 0, textAsBlob('{"prefix": "enwiki", "title": "\"Aghnadarragh\""}'), 100
+ * );
+ * <p>
+ * insert into test_by_score (commit, delta, test, score) values (
+ * textAsBlob('adb14fbe076f6b94444c660e36a400151f26fc6f'), 0, textAsBlob('{"prefix": "enwiki", "title": "\"Aghnadarragh\""}'), 100
+ * );
  */
 
 
@@ -62,12 +64,15 @@ public class DataStore {
     private String bucket;
     @Column
     private Set tags = new HashSet();
+    @Column
+    private ByteBuffer image;
 
-    public DataStore(UUID id, String type, String bucket, Set tags) {
+    public DataStore(UUID id, String type, String bucket, Set tags, ByteBuffer image) {
         this.id = id;
         this.type = type;
         this.bucket = bucket;
         this.tags.addAll(tags);
+        this.image = image;
     }
 
     public UUID getId() {
@@ -84,5 +89,9 @@ public class DataStore {
 
     public Set getTags() {
         return tags;
+    }
+
+    public ByteBuffer getImage() {
+        return image;
     }
 }
